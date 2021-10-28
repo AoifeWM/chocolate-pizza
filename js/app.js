09d696212547b0ec2dc3ef8631418f9b5cb67f7a
AoifeWM <aoifewmulligan@gmail.com>
2021-10-27 19:19:26 -0700

    added new style and content up to the list portion

--- js/app.js
+++ js/app.js
@@ -1,10 +1,22 @@
 'use strict'
 
 let banner = document.getElementById('banner');
+let banner2 = document.getElementById('banner2');
 
 let imageString = "";
 for (let i = 0; i < 930; i++){
     imageString += '<img src="img/hr-img.png" alt="">';
 }
 
-banner.innerHTML = imageString
+banner.innerHTML = imageString;
+banner2.innerHTML = imageString;
+
+let printButton = document.getElementById('print');
+
+console.log(printButton);
+
+function printFunction(){
+    window.print();
+}
+
+printButton.addEventListener('click', printFunction);
